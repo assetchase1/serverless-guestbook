7ded82e0d3aabbdeae844fbd8df1edad73949170
assetchase1 <121323206+assetchase1@users.noreply.github.com>
2022-12-31 19:35:27 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/437648cd-b88c-4831-9d93-1ff06c70cac3/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/https://us-south.functions.appdomain.cloud/api/v1/web/437648cd-b88c-4831-9d93-1ff06c70cac3/guestbook/.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/https://us-south.functions.appdomain.cloud/api/v1/web/437648cd-b88c-4831-9d93-1ff06c70cac3/guestbook/.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
